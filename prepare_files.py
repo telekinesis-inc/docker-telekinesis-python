a3b8a695e3f74da05694bf87785c375d5040f405
Elias <elias@telekinesis.cloud>
2021-09-27 19:49:56 +0200

    Made prepare_files paths relative to __file__

--- prepare_files.py
+++ prepare_files.py
@@ -1,15 +1,18 @@
-def prepare_files(*dependencies):
+import os
 
-    with open('Dockerfile_base', 'r') as file_in:
+path = os.path.dirname(os.path.abspath(__file__))
+
+def prepare_files(*dependencies):
+    with open(os.path.join(path, 'Dockerfile_base'), 'r') as file_in:
         base = file_in.read()
         out = base.replace('{{DEPENDENCIES}}', '\n'.join('RUN pip install '+ d for d in dependencies))
 
-        with open('Dockerfile', 'w') as file_out:
+        with open(os.path.join(path, 'Dockerfile'), 'w') as file_out:
             file_out.write(out)
 
-    with open('script_base.py', 'r') as file_in:
+    with open(os.path.join(path, 'script_base.py'), 'r') as file_in:
         base = file_in.read()
         out = base.replace('{{IMPORTS}}', '\n'.join('import '+ d for d in dependencies))
 
-        with open('script.py', 'w') as file_out:
+        with open(os.path.join(path, 'script.py'), 'w') as file_out:
             file_out.write(out)
