98273b7e28c1ed60bedad61555b74c42193d3b72
Elias <elias@telekinesis.cloud>
2021-12-12 16:23:08 +0100

    Updated script_base

--- script_base.py
+++ script_base.py
@@ -4,35 +4,28 @@ import os
 import json
 {{IMPORTS}}
 
-class Dict(dict):
-    def update(*args):
-        raise PermissionError
-    def pop(*args):
-        raise PermissionError
-    def popitem(*args):
-        raise PermissionError
-    def clear(*args):
-        raise PermissionError
-
-async def executor(code, namespace=None):
-    namespace = namespace or {}
-    prefix = 'async def _wrapper(_new):\n'
-    content = ('\n'+code).replace('\n', '\n    ')
-    suffix = """
-    for _var in dir():
-        if _var[0] != '_':
-            _new[_var] = eval(_var)"""
-    exec(prefix+content+suffix, namespace)
-    new_vars = {}
-    await namespace['_wrapper'](new_vars)
-    return Dict(new_vars)
-
 async def main():
     print('starting')
     e = asyncio.Event()
 
+    async def executor(code, namespace=None, inject_context=False):
+        namespace = namespace or {}
+        if inject_context:
+            namespace['_context'] = {'stop': lambda: e.set()}
+        prefix = 'async def _wrapper(_new):\n'
+        content = ('\n'+code).replace('\n', '\n    ')
+        suffix = """
+    for _var in dir():
+        if _var[0] != '_':
+            _new[_var] = eval(_var)"""
+        exec(prefix+content+suffix, namespace)
+        new_vars = {}
+        await namespace['_wrapper'](new_vars)
+        return new_vars
+
+
     with open('../session_key.pem', 'w') as file:
-        file.write(os.environ['PRIVATEKEY'])
+        file.write(os.environ['PRIVATEKEY'].replace('\\', '\n'))
 
     entrypoint = await tk.Entrypoint('ws://127.0.0.1:8777', '../session_key.pem')
     route = tk.Route(**json.loads(os.environ['ROUTE']))
